a97e0a08e68c4d451d841f3981b8017d2c835475
Letícia Araujo <67714382+leticiafdearaujo@users.noreply.github.com>
2020-07-07 11:03:59 -0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,4 +1,5 @@
---Soma as calorias de uma tabela agrupando por tipo e onde a soma for maior que 150:
+--Soma as calorias de uma tabela agrupando por tipo e onde a soma for maior que 150. O Having funciona para garantir que seja aplicado o filtro após a somatória, 
+--pois se utilizasse o WHERE ele olharia por linha
 SELECT type, 
 SUM(calories) AS total_calories 
 FROM exercise_logs
@@ -26,3 +27,54 @@ end as "letter_grade"
 from student_grades
 
 group by letter_grade;
+
+-- Case com mais de um critério
+select count(country) as qnt,
+case 
+    when country in ("Israel", "South Africa") then "Africa"
+    when country in ("United states", "Canada") then "América"
+    when country in ("India", "United Arab Emirates") then "Asia"
+else "Outros"
+end as "Continente"
+from sales
+group by Continente
+
+---Uso do or
+
+select count(ID) as qnt_vendida
+from sales
+where country = "Norway" or country = "France";
+
+-- INNER JOIN -- Tudo que tem em comum nas duas tabelas
+select 
+persons.name, 
+hobbies.name 
+from persons 
+join hobbies on persons.id = hobbies.person_id;
+
+select 
+persons.name, 
+hobbies.name 
+from persons 
+join hobbies on persons.id = hobbies.person_id 
+where persons.name = "Bobby McBobbyFace";
+
+-- LEFT OUTER JOIN -- Tudo que tem na Tabela da Direita (From) acrescentando os dados da Tabela da Esquerda
+SELECT students.first_name, students.last_name, student_projects.title
+    FROM students
+    LEFT OUTER JOIN student_projects
+    ON students.id = student_projects.student_id;
+    
+-- LEFT OUTER JOIN -- Tudo que tem na Tabela da Direita (From) acrescentando os dados da Tabela da Esquerda (Subqueries)
+select
+name,
+mail,
+sum (price) as total
+from
+(select 
+customers.name as name, customers.email as mail, orders.price as price
+from customers 
+LEFT OUTER JOIN orders on customers.id = orders.customer_id)
+group by name, mail
+order by total desc;
+
